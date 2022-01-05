536ad50b8d5f8ab4d75bcba4eceb4f5695dd842e
kattycreates <iamkarthika7@gmail.com>
2022-01-05 16:37:27 +0530

    modifies files

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,9 +3,7 @@ import React from 'react'
 const Home = () => {
     return (
         <div id="home">
-            <div className="picture">
-                <img src='assets/nerdlisa.png' alt='illustrastion'/>
-            </div>
+            
             <div className="text">
                 <p>Hello,</p>
                 <p>I am <span>Karthika!</span></p>
